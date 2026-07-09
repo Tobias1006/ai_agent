579240f7ce0dff0d55052c8ade727e859d3d65e8
Tobias1006 <tgoettsche0712@gmail.com>
2026-07-09 15:28:16 +0200

    Working get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,15 +2,24 @@ import os
 
 def get_files_info(working_directory:str, directory: str = ".") -> str:
     try:
+        ent_det = []
         path_work_dir = os.path.abspath(working_directory)
         full_path = os.path.normpath(os.path.join(path_work_dir, directory))
         path_list = [full_path, path_work_dir]
         valid_target_dir = os.path.commonpath(path_list) == path_work_dir
+        list_of_files = os.listdir(full_path)        
+
         if not valid_target_dir: 
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(full_path):
-            return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
+            return f'Error: "{directory}" is not a directory'        
+        for ent in list_of_files:
+            path_ent = os.path.join(full_path, ent)
+            ent_det.append((path_ent, os.path.getsize(path_ent), os.path.isdir(path_ent)))
+            print(ent_det)
+        for ent in ent_det: 
+            return f'- {ent[0]}: file_size={ent[1]}, is_dir={ent[2]}'
+        
     except Exception as e:
             return f'Error: {e}'
 
