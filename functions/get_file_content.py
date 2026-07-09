714fc3bd01ff237cba5d95c548b468de7a88e4c1
Tobias1006 <tgoettsche0712@gmail.com>
2026-07-09 18:40:23 +0200

    get_file_content needs fixing

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+from functions.config import MAX_CHARACTERS
+
+def get_file_content(working_directory: str, file_path:str) -> str:
+    try: 
+        path_work_dir = os.path.abspath(working_directory)
+        full_path = os.path.normpath(os.path.join(path_work_dir, file_path))
+        path_list = [full_path, path_work_dir]
+        valid_target_file = os.path.commonpath(path_list) == path_work_dir
+
+        if not valid_target_file: 
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(full_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        open(full_path)
+        print(full_path)
+        
+        with open(file_path, "r") as f:
+            file_content = f.read(MAX_CHARACTERS)
+        if full_path.read(1):
+            file_content += f'[...File "{file_path}" truncated at {MAX_CHARACTERS} characters]'
+        
+        
+    except Exception as e:
+            return f'Error: {e}'
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,18 +1,21 @@
 import os 
+from functions.path_validation import validate_path
 
 def get_files_info(working_directory:str, directory: str = ".") -> str:
     try:
         ent_det = []
+        
         path_work_dir = os.path.abspath(working_directory)
         full_path = os.path.normpath(os.path.join(path_work_dir, directory))
         path_list = [full_path, path_work_dir]
         valid_target_dir = os.path.commonpath(path_list) == path_work_dir
-        list_of_files = os.listdir(full_path)        
+        
+        list_of_files = os.listdir(full_path) 
 
         if not valid_target_dir: 
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(full_path):
-            return f'Error: "{directory}" is not a directory'        
+            return f'Error: "{directory}" is not a directory'
         for ent in list_of_files:
             path_ent = os.path.join(full_path, ent)
             ent_det.append((path_ent, os.path.getsize(path_ent), os.path.isdir(path_ent)))
